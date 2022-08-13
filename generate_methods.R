cb22036750b21e2ac89bea78a590160eb455035c
matthewhirschey <matthew@hirschey.org>
2022-08-12 20:53:31 -0400

    remove function
    just run script

--- generate_methods.R
+++ generate_methods.R
@@ -1,63 +1,60 @@
 source("fun_libraries.R") #used by fun.R too
 Sys.setenv(VROOM_CONNECTION_SIZE = "1310720") #10x bigger; errors out otherwise
 
-generate_methods <- function(){
-  #generate methods via quarto
-  quarto::quarto_render(input = here::here("quarto"), #expecting a dir to render
-                        output_format = "html", #output dir is set in _quarto.yml
-                        cache_refresh = TRUE)
-  
-  #ZIP
-  #make tempdir our working directory
-  owd <- getwd()
-  setwd(here::here("quarto")) #for methods zipping
-  on.exit(setwd(owd))
-  
-  methods_files <- list.files(here::here("quarto", "methods"), 
-                              recursive = TRUE, 
-                              include.dirs = TRUE)
-  
-  message(glue::glue_collapse(methods_files, sep = ", ")) #just for confirmation
-  
-  #zip
-  final_zip_path <- glue::glue("{owd}/methods.zip")
-  zip(zipfile = final_zip_path, 
-      files = glue::glue("methods/{methods_files}"))
-  
-  #move to S3 as www/methods.zip
-  s3 <- paws::s3()
-  s3$put_object(
-    Body = as.character(final_zip_path),
-    Bucket = Sys.getenv("AWS_DATA_BUCKET_ID"),
-    Key = "www/methods.zip"
-  )
-  complete_message <- "Hey, you just uploaded fresh methods to S3...nice work!"
-  message(complete_message)
-  
-  #send email to spin down container
-  email_body <- compose_email(
-    header = add_image(file = here::here("ddh-banner.png"), width = "100%"), 
-    body = complete_message)
-  
-  email_body <- add_attachment(email = email_body,
-                               file = final_zip_path)
-  
-  email_body %>%
-    smtp_send(
-      from = "hey@datadrivenhypothesis.com",
-      to = "matthew@hirschey.org",
-      subject = "Methods completed",
-      credentials = creds_envvar(
-        user = Sys.getenv("SMTP_USERNAME"),
-        #pass_envvar = Sys.getenv("SMTP_PASSWORD"), #use default
-        host = "mail.privateemail.com",
-        port = "465",
-        use_ssl = TRUE
-      )
-    )
-}
+#generate methods via quarto
+quarto::quarto_render(input = here::here("quarto"), #expecting a dir to render
+                      output_format = "html", #output dir is set in _quarto.yml
+                      cache_refresh = TRUE)
 
-generate_methods()
+#ZIP
+methods_files <- list.files(here::here("quarto", "methods"), 
+                            recursive = TRUE, 
+                            include.dirs = TRUE)
+
+message(glue::glue_collapse(methods_files, sep = ", ")) #just for confirmation
+
+#make tempdir our working directory
+owd <- getwd()
+setwd(here::here("quarto")) #for methods zipping
+on.exit(setwd(owd))
+
+#zip
+final_zip_path <- glue::glue("{owd}/methods.zip")
+zip(zipfile = final_zip_path, 
+    files = glue::glue("methods/{methods_files}"))
+
+#move to S3 as www/methods.zip
+s3 <- paws::s3()
+s3$put_object(
+  Body = as.character(final_zip_path),
+  Bucket = Sys.getenv("AWS_DATA_BUCKET_ID"),
+  Key = "www/methods.zip"
+)
+complete_message <- "Hey, you just uploaded fresh methods to S3...nice work!"
+message(complete_message)
+
+#send email to spin down container
+email_body <- compose_email(
+  header = add_image(file = here::here("ddh-banner.png"), width = "100%"), 
+  body = complete_message)
+
+email_body <- add_attachment(email = email_body,
+                             file = final_zip_path)
+
+email_body %>%
+  smtp_send(
+    from = "hey@datadrivenhypothesis.com",
+    to = "matthew.hirschey@duke.edu",
+    subject = "Methods completed",
+    credentials = creds_envvar(
+      user = Sys.getenv("SMTP_USERNAME"),
+      #pass_envvar = Sys.getenv("SMTP_PASSWORD"), #use default
+      host = "mail.privateemail.com",
+      port = "465",
+      use_ssl = TRUE
+    )
+  )
+
 
 
 
