a26490856e57c881e318164df27c6df2b457e2d9
John Bradley <johnbradley2008@gmail.com>
2023-06-27 11:28:27 -0400

    Update messages

--- runtime.R
+++ runtime.R
@@ -4,7 +4,7 @@ source("fun.R")
 get_public_reports()
 
 generate_and_email_report <- function(details) {
-  message("generate email for ", details['email_address'], " content:", details['content'])
+  message("generating email for ", details['email_address'], " content:", details['content'])
   send_email(first_name = details['first_name'],
              email_address = details['email_address'],
              input = list(type = details['type'], 
@@ -13,16 +13,15 @@ generate_and_email_report <- function(details) {
                           content = details['content']),
              private = as.logical(details['private']), 
              greeting = details['greeting'])
+  message("after sending email")
 }
 
 process_message_body <- function(message_body) {
-  message("process_message_body")
   report_details_df <- jsonlite::fromJSON(message_body)
   apply(report_details_df, 1, generate_and_email_report)
 }
 
 process_sqs <- function(Records) {
-  message("process_sqs")
   lapply(Records$body, process_message_body)
   "ok"
 }
